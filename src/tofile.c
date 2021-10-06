4430fa1516c7dab10c6d90557401a05f221ca9ab
Roman Lecomte <romanlecomte@gmail.com>
2021-10-06 11:13:41 +0200

    [correct] debug de ce fichier

--- src/tofile.c
+++ src/tofile.c
@@ -1,16 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void tofile(int sorted[], double executionTime[]){
+void tofile(int sorted[]){
     FILE* file = NULL;
 
     file = fopen("result.csv", "a+");
     if(file != NULL){
         // Test ouverture fichier et action réalisées
-        size_t lenSorted = sizeof(sorted)/sizeof(int);    
-        size_t lenExec = sizeof(executionTime)/sizeof(double);
+        size_t lenSorted = sizeof(sorted)/sizeof(int);
+        //size_t lenExec = sizeof(executionTime)/sizeof(double);  double executionTime[]
 
-        fputs("Tableaux triés\n");
+        fputs("Tableaux triés\n", file);
         for(int i=0; i<lenSorted;i++){
             fputc(sorted[i], file);
             fputc('\n', file);
