fc6c27ac73d50b6bb07435143c38353a9b597c56
AlexisVillaroya <alexisvillaroya2a@gmail.com>
2021-10-08 09:40:14 +0200

    [feat] random number and selection

--- src/main.c
+++ src/main.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "triSelection/triSelection.h"
 
+float *randomFloatArray(int tabsize);
+
 int main()
-<<<<<<< HEAD
-{	
-	
-=======
 {
-	printf("Hello World!\n");
-	tri_a_bulle();
-	return 0;
->>>>>>> 0066a1ebb814715c7382c937293b927ecbc7090d
+	//tri_a_bulle();
+
+	// tri par sélection 
+	float *tab;
+	tab = randomFloatArray(7);
+	for(int i = 0; i < 7 ;i++){
+		printf("%.0f ", tab[i]);
+	}
+	printf("\n");
+	tab = triSelection(tab, 7);
+	for(int i = 0; i < 7 ;i++){
+		printf("%.0f ", tab[i]);
+	}
+	printf("\n");
+	// tri à bulle
+
+
+
+	// tri par insertion 
+
+
+}
+
+/* Génération d'une chaine de float aléatoire */
+float *randomFloatArray(int tabsize){ 
+	float *tabFloat = malloc(sizeof(float)*tabsize);
+	float a = 10000000.0;
+	for(int i=0; i<tabsize; i++){
+		tabFloat[i] = ((float)rand()/(float)(RAND_MAX)*a);
+	}
+	return tabFloat;
 }
--- src/tofile.c
+++ src/tofile.c
@@ -1,32 +1,32 @@
-#include <stdio.h>
-#include "tofile.c"
+// #include <stdio.h>
+// #include "tofile.c"
 
-int main()
-{
-	int test[] = { 1,2,3,4,5 };
-    FILE* file = NULL;
+// int main()
+// {
+// 	int test[] = { 1,2,3,4,5 };
+//     FILE* file = NULL;
 
-    file = fopen("result.csv", "a+");
-    if(file != NULL){
-        // Test ouverture fichier et action réalisées
-        int lenSorted = (int) sizeof(test)/sizeof(int);
-        //size_t lenExec = sizeof(executionTime)/sizeof(double);  double executionTime[]
+//     file = fopen("result.csv", "a+");
+//     if(file != NULL){
+//         // Test ouverture fichier et action réalisées
+//         int lenSorted = (int) sizeof(test)/sizeof(int);
+//         //size_t lenExec = sizeof(executionTime)/sizeof(double);  double executionTime[]
 
-        fputs("Tableaux triés;Sortie m/s", file);
-        for(int i=0; i<lenSorted;i++){
-            fprintf(file,"%d", test[i]);
-        }
-		// TODO
-		/*
-			replacer le curseur en haut du fichier
-			sauter une ligne
-			déclaer avec ; 
+//         fputs("Tableaux triés;Sortie m/s", file);
+//         for(int i=0; i<lenSorted;i++){
+//             fprintf(file,"%d", test[i]);
+//         }
+// 		// TODO
+// 		/*
+// 			replacer le curseur en haut du fichier
+// 			sauter une ligne
+// 			déclaer avec ; 
 
-		*/
-		for(int i=0; i<lenSorted;i++){
-            fprintf(file,";%d", test[i]);
-        }
-    }else{
-        printf("L'ouverture du fichier a echouee..");
-    }
-}
+// 		*/
+// 		for(int i=0; i<lenSorted;i++){
+//             fprintf(file,";%d", test[i]);
+//         }
+//     }else{
+//         printf("L'ouverture du fichier a echouee..");
+//     }
+// }
