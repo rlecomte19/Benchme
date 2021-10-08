71f3d8674e8d48da0de425d512712800d96ac393
Roman Lecomte <romanlecomte@gmail.com>
2021-10-08 11:55:36 +0200

    INSERTION

---
+++ src/triInsertion/triInsertion.c
@@ -0,0 +1,18 @@
+float *triInsertion(float *tableau, int size){
+    float tab[size];
+    for(int i=0;i<size;i++){
+        tab[i] = tableau[i];
+    }
+    
+    for(int i=0;i<size;i++){
+        int x = tab[i];
+        int j = i;
+        while(j > 0 && tab[j-1] > x){
+            tab[j] = tab[j-1];
+            j--;
+        }
+        tab[j] = x;
+    }
+
+    return tab;
+}
---
+++ src/triInsertion/triInsertion.h
@@ -0,0 +1,1 @@
+float *triInsertion(float *tableau, int size);
