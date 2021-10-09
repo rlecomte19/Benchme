d942f5de9e9a196a005a4d9abcbdf6977deba211
AlexisVillaroya <alexisvillaroya2a@gmail.com>
2021-10-09 17:47:10 +0200

    Correctifs main

--- src/main.c
+++ src/main.c
@@ -1,58 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "triSelection/triSelection.h"
-#include "tofile/tofile.h"
-#include "tri_a_bulle/tri_a_bulle.h"
-#include "test/test.h"
-
-float *randomFloatArray(int tabsize);
+#include "./tofile/tofile.h"
 
 int main()
 {
-	
-	float test[] = { 1,2,3,4.2 };
+	float arr1[100];
+	float arr2[1000];
+	float arr3[10000];
+	float arr4[100000];
+	float arr5[1000000];
+	float arr6[10000000];
+	int *tabSizes = { 100, 1000, 10000, 100000, 1000000, 10000000};
+	float *listArrays = { arr1, arr2, arr3, arr4, arr5, arr6 }; 
 
-	//getCpu(test, timeTaken);
-
-
-	int lenSorted = (int) sizeof(test)/sizeof(float);
-	tofile(test, lenSorted);
-	printf("Hello World!\n");
-	//tri_a_bulle();
-
-	float *tab;
-	tab = randomFloatArray(7);
-
-	
-	// tri par sélection 
-	for(int i = 0; i < 7 ;i++){
-		tab = randomFloatArray(10);
-	}
-	for(int i = 0; i < 10 ;i++){
-		printf("%.0f ", tab[i]);
-	}
-	printf("\n");
-	tab = triSelection(tab, 10);
-	for(int i = 0; i < 10 ;i++){
-		printf("%.0f ", tab[i]);
-	}
-	printf("\n");
-	
-	/*
-	// tri à bulle
-	float *tabResult = malloc(sizeof(float)*7);
-	memcpy(tabResult, tab, sizeof(float)*7);
-	
-	tri_a_bulle(tabResult, 7);
-	
-	for(int nJ=0; nJ<7; nJ++)
-        printf("%.0f - ", tab[nJ]);
-	printf("\n");
-	for(int nJ=0; nJ<7; nJ++)
-        printf("%.0f - ", tabResult[nJ]);
-	*/
-	
-	// tri par insertion 
+	// Remplissage aléatoire des tableaux de différent nombre de valeurs
+	randomFloat(tabSizes, listArrays);
 
 
 }
--- src/triSelection/triSelection.c
+++ src/triSelection/triSelection.c
@@ -18,5 +18,4 @@ float *triSelection(float *tableau, int size){
     }
     return tab;
 
-    
 }
