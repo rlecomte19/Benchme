33c72c13d0ea3b2b892b307776f6cf9fc38b2ec2
AlexisVillaroya <alexisvillaroya2a@gmail.com>
2021-10-10 17:20:58 +0200

    [edit] modification fonction random

--- src/test/test.c
+++ src/test/test.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @brief Affiche tableau
+ * 
+ * @param tableau Tableau de float
+ * @param size Taille du tableau
+ */
+
 void diplayArray(float *tableau, int size){
     for (int i = 0; i < size; i++)
 	{
@@ -8,6 +15,15 @@ void diplayArray(float *tableau, int size){
 	}
 	printf("\n");
 }
+
+/**
+ * @brief Remplit des tableaux avec des valeurs aléatoire 
+ * 
+ * @param listArrays Liste des tableaux à remplir  
+ * @param tabsizes Liste des tailles des tableaux à remplir
+ * @param sizeListArray Taille de ListArrays
+ */
+
 void *randomFloatArray(float **listArrays, int *tabsizes, int sizeListArray){ 
 	float a = 10000000.0;
 	for(int i=0; i<sizeListArray; i++){
@@ -17,6 +33,14 @@ void *randomFloatArray(float **listArrays, int *tabsizes, int sizeListArray){
 		}
 	}	
 }
+
+/**
+ * @brief Effectue un test de tri 
+ * 
+ * @param listArray Liste des tableaux 
+ * @param tabSizes Nombre de valeurs dans le tableau
+ * @return Tableau trié
+ */
 
 void tri_test(float *listArray[], float (*func)(float*,int), int *tabSizes){
 	for (int i=0;i<=5;i++){
